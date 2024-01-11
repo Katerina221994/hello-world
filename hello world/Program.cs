7b1d38c1977920d22084ca05fb39803e56e18357
Ekaterina <rfvb.foto@mail.ru>
2024-01-11 16:48:12 +0200

    Добавьте файлы проекта.

---
+++ hello world/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using static System.Console;
+
+
+namespace hello_world
+{
+    internal class Program
+    { 
+        /// <summary>
+        /// код который выпоняется в первую очередь
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!!");
+            ReadKey(); //приложение не закроется пока небудет нажата какая нибудь кнопка
+          
+        }
+    }
+}
